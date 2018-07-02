ac8d4d056a878083951c9456a889eddca7eb5dba
MGonzalezx <40763643+MGonzalezx@users.noreply.github.com>
2018-07-02 17:06:32 +0200

    Ejercicios del curso
    los primeros 3

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,16 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
-y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function mostrar()
-{
-	
+
+{   //var person = prompt("Please enter your name", "Harry Potter");
+	/*Lo sacamos de w3schools
+	lo vamos a usar como ejemplo
+	*/
+  var nombre;
+  nombre=prompt("Ingrese su nombre","Nombre Apellido");
+  alert("Su nombre es: "+nombre);
+
+
+
 }
 
--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -1,8 +1,14 @@
 /*Debemos lograr tomar un dato por 'ID'
-y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
+y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
 function mostrar()
-{
-	
+{ /*console.log () 
+	alert ("ejercicio 3");
+	*/
+	var nombre;
+	nombre=document.getElementById('elNombre').value;
+    //Para hacer lo de arriba, pongo get luego apreto tab cuando esta lo azul abajo, y luego apreto shift+i
+   alert (nombre);
 }
+
 
 
