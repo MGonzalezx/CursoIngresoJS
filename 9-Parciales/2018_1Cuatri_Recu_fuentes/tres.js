5179ceb323ee6548ed6eb29a37e9a442aaa008fc
MGonzalezx <40763643+MGonzalezx@users.noreply.github.com>
2018-07-04 12:23:09 -0300

    parcial recu

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,30 @@
 function mostrar()
 {
+   var precio;
+   var porcentajeDeDescuento;
+   var precioConDescuento;
+   var precioConDescuentoMasIva;
+   var iva;
+   var cantidadDescontada;
+   
+   precio = prompt("Ingrese precio");
+   porcentajeDeDescuento = prompt("Ingrese Porcentaje a descontar");
+
+    cantidadDescontada = (porcentajeDeDescuento*precio)/100 ;
+    precioConDescuento = precio-((porcentajeDeDescuento*precio)/100) ;
+    iva = (21*precioConDescuento)/100 ;
+    precioConDescuentoMasIva = precioConDescuento + iva ;
+
+
+  
+
+   alert("Se descuenta: "+ cantidadDescontada + ". Precio con descuento "+ precioConDescuento+ ". El iva del precio con descuento: " + iva);
+
+   document.getElementById('elPrecioFinal').value = precioConDescuentoMasIva ;
 
 }
+
+
+
+
+
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,17 @@
 
 function mostrar()
 {
+	var base;
+	var altura;
+	var superficie;
+	var perimetro;
+
+	base = prompt("Ingrese base del triangulo");
+	altura = prompt("Ingrese altura del triangulo");
+
+	perimetro = base*3 ;
+	superficie = (base*altura)/2 ;
+
+	alert ("La superficie es "+ superficie+ " y el perimetro es "+ perimetro) ;
 
 }
