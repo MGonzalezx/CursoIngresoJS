e95a025f7112612a0a580ab4f6de187ebf13534c
MGonzalezx <40763643+MGonzalezx@users.noreply.github.com>
2018-07-23 12:18:57 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,21 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var nota ;
+
+	nota = Math.round(Math.random()*10) ;
+
+	if (nota >= 9 && nota <= 10)
+	{
+		alert("Excelente " + nota);
+	}
+	else if (nota > 4 && nota <= 8)
+	{
+		alert("Aprobò " + nota);
+	}
+	else 
+	{
+		alert("Vamos, la proxima se puede " + nota);
+	}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,7 +1,11 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var numeroRandom ;
+
+	numeroRandom = Math.round(Math.random(1)*10);
+
+	alert(numeroRandom);
 	
 
 }//FIN DE LA FUNCIÓN
