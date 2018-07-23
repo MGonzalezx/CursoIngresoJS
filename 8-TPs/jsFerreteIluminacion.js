6ba4239737445c2e72e5f54aa10a06143899adcd
MGonzalezx <40763643+MGonzalezx@users.noreply.github.com>
2018-07-23 12:19:39 -0300

    Add files via upload

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -10,5 +10,119 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  */
 function CalcularPrecio () 
 {
+ 	var cantidad ;
+ 	var marca ;
+ 	var precioLampara;
+ 	var precioSinDescuento ;
+ 	var descuento ;
+ 	var precioFinal ;
  	
+ 	cantidad = document.getElementById('Cantidad').value ;
+ 	marca = document.getElementById('Marca').value ;
+
+ 	precioLampara = 35 ;
+ 	descuento = 1 ;
+ 	precioSinDescuento = cantidad * precioLampara ;
+
+ 	cantidad =parseInt(cantidad, 0) ;
+
+ 	switch(cantidad)
+ 	{
+ 		case 1:
+ 		case 2:
+ 			break;
+ 		case 3 :	
+			switch(marca)
+		{
+			case "ArgentinaLuz":
+			descuento = 0.85;
+			case "FelipeLamparas":
+			descuento = 0.90;
+			default:
+			descuento = 0.95 ;
+				break;
+			 }
+			case 4:
+			switch(marca)
+			{
+			case "ArgentinaLuz":
+				descuento = 0.75
+			case "FelipeLamparas":
+				descuento = 0.75	
+			}
+
+	}		
+ 	    
+
+ 	   precioFinal = precioSinDescuento * descuento ;
+
+ 	   document.getElementById('precioDescuento').value = precioFinal
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 	/* if (cantidad >= 6)
+ 	{
+ 		alert(precioSinDescuento - (50*precioSinDescuento/100));
+ 	}
+ 	else 
+ 		if (cantidad == 5)
+ 		{
+ 			if (marca != "ArgentinaLuz")
+ 			{
+ 				alert(precioSinDescuento - (30*precioSinDescuento/100)) ;
+ 			}
+ 		   
+ 		   if (marca == "ArgentinaLuz")
+ 		   {
+ 		   		alert(precioSinDescuento - (40*precioSinDescuento/100)) ;
+ 		   }	
+
+ 	else 
+ 		if (cantidad == 4)
+ 		{
+ 			if (marca != "ArgentinaLuz" || marca != "FelipeLamparas")
+ 			{
+ 				alert(precioSinDescuento - (20*precioSinDescuento/100)) ;
+ 			}
+
+ 			if (marca == "ArgentinaLuz" || marca == "FelipeLamparas")
+ 			{
+ 				alert(precioSinDescuento - (25*precioSinDescuento/100)) ;
+ 			}
+ 		} 
+
+ 	else
+ 		if (cantidad == 3)
+ 		{
+ 			alert(precioSinDescuento - (5*precioSinDescuento/100)) ;
+
+ 			if (marca == "ArgentinaLuz")
+ 			{
+ 				alert(precioSinDescuento - (15*precioSinDescuento/100)) ;
+ 			}
+
+ 			if (marca == "FelipeLamparas")
+ 			{
+ 				alert(precioSinDescuento - (10*precioSinDescuento/100)) ;
+ 			}
+ 		}
+
+
+ 		}
+
+
+
+*/
 }
