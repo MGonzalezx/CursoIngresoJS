ca06b2a2131c33f5de260dbe460eb808282ec617
MGonzalezx <40763643+MGonzalezx@users.noreply.github.com>
2018-07-23 19:47:28 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,29 @@
 function mostrar()
-{
+{ var numeroUno ;
+  var numeroDos ;
+  
+  numeroUno = prompt("Ingrese primer numero") ;
+  numeroDos = prompt("Ingrese segundo numero") ;
 
+  suma =parseInt(numeroUno, 0) + parseInt(numeroDos, 0) ;
+
+  if(numeroUno == numeroDos)
+  {
+	alert(numeroUno + numeroDos);
+  } 
+  else if(suma > 10)
+  {
+  	alert("La suma es: "+suma+" y supero el 10"); 
+  }
+  else if(numeroUno < numeroDos)
+  {
+  	alert(suma);
+  }
+  else
+  {
+  	alert(numeroUno - numeroDos)
+  }
+
+// Primero prompt despues parseInt
+  
 }
