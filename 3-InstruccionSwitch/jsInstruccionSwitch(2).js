83a005734c6dc270897fdb9fa67a28ebaadc247a
MGonzalezx <40763643+MGonzalezx@users.noreply.github.com>
2018-07-25 08:36:19 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,9 +1,65 @@
 function mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	//tomo la edad  
+	var mesDelAño = document.getElementById('mes').value;
+		
+	switch (mesDelAño)
+	{
+		case "Enero":
+			alert("Falta para el invierno.");
+			break ;
+		case "Febrero":
+			alert("Falta para el invierno.");
+			break ;
+		case "Marzo":
+			alert("Falta para el invierno.");
+			break ;
+		case "Abril":
+			alert("Falta para el invierno.");
+			break ;
+		case "Mayo":
+			alert("Falta para el invierno.");
+			break ;
+		case "Junio":
+			alert("Falta para el invierno.");
+			break ;
+		case "Julio":
+			alert("Abrigate que hace frio.");
+			break ;
+		case "Agosto":
+			alert("Abrigate que hace frio.");
+			break ;			
+		case "Septiembre":
+			alert("Ya pasamos el frio, ahora calor!!!.");
+			break ;
+		case "Octubre":
+			alert("Ya pasamos el frio, ahora calor!!!.");
+			break ;
+		case "Noviembre":
+			alert("Ya pasamos el frio, ahora calor!!!.");
+			break ;
+		case "Diciembre":
+			alert("Ya pasamos el frio, ahora calor!!!.");
+			break ;
+	}	
 
-alert (mesDelAño);
+//alert (mesDelAño);
+
+/* tambien para hacerlo màs rapido y sin repetir siempre el mismo alert podemos hacer esto
+		case "Julio":
+
+		case "Agosto":
+			alert("Abrigate que hace frio.");
+			break ;			
+Se termina el break, toma que en julio O Agosto ocurre el alert
+
+El else del switch es default. 
+       	default :
+       	alert("Ya pasamos el frio, ahora calor!!!.") ;
+Si es que antes ya pusimos los meses anteriores al invierno y los meses de invierno. 
+
+
+*/
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,7 +1,42 @@
 function mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	//tomo la edad  
+	var mesDelAño = document.getElementById('mes').value;
+
+	switch(mesDelAño)
+	{
+		case "Enero":
+
+		case "Marzo":
+
+		case "Mayo":
+
+		case "Julio":
+
+		case "Agosto":
+
+		case "Octubre":
+
+		case "Diciembre":
+			alert("si tiene 31 días.");
+			break ;
+
+		case "Abril":
+
+		case "Junio":
+
+		case "Septiembre":
+
+		case "Noviembre":
+			alert("si tiene 30 días.");
+			break ;
+
+		default :
+		alert("si tiene 28 días.");
+
+
+
+	}
 
 //alert (mesDelAño);
 	
