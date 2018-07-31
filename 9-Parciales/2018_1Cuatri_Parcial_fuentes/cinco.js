124cbc3caf6f7c781a164f33cb069ea464a8cf87
MGonzalezx <40763643+MGonzalezx@users.noreply.github.com>
2018-07-30 21:08:45 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,24 @@
 function mostrar()
 {
+	var planeta ;
 
+	planeta = prompt("Ingrese nombre del planeta") ;
+
+	switch(planeta)
+	{
+		case "mercurio":
+		case "venus":
+			alert("Aca hace más calor");
+			break;
+		case "tierra":
+			alert("Aca vivimos");
+			break;
+		case "marte":
+		case "jupiter":
+			alert("Aca hace más frio");
+			break;
+		default:
+			alert("No es un planeta valido");
+			break;
+	}
 }
