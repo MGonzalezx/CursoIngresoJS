52112b91980a72057ed8af4627274969e95a73e5
MGonzalezx <40763643+MGonzalezx@users.noreply.github.com>
2018-08-06 12:03:53 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,6 +1,68 @@
 function mostrar()
-{
+{	//Numero primo
+	var numeroIngresado ;
+	var numerosAnteriores ;
+	var esPrimo ;
+	var numeroRecorrido ;
 
+
+	numeroIngresado = prompt("Ingrese numero");
+	numeroIngresado = parseInt(numeroIngresado);
+
+	for(numeroRecorrido = numeroIngresado ; numeroRecorrido > 1 ; numeroRecorrido--)
+	{
+		esPrimo = 0 ;
+	
+		for(numerosAnteriores = 2 ; numerosAnteriores < numeroRecorrido ; numerosAnteriores++)
+		{
+		
+			if(numeroRecorrido % numerosAnteriores == 0)
+		{
+			
+			esPrimo = 1 ;
+			break ;
+		}
+		}
+
+		if(esPrimo == 0)
+	{
+		console.log(numeroRecorrido + " Es un numero primo");
+	}
+	
+	}
+	
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+	/* for(numerosAnteriores - numerosIngresados - 1 ; numerosAnteriores > 1 ; numerosAnteriores--)
+	{
+		
+		if(numeroIngresado % numerosAnteriores == 0)
+		{
+			console.log(numerosAnteriores);
+			esPrimo = 1 ;
+			break ;
+		}
+	}
+
+	if(esPrimo == 0)
+	{
+		console.log(numeroIngresado + " Es un numero primo");
+	}
+	else
+	{
+		console.log(numeroIngresado + " No es un numero primo");
+	} */
 
 
 
